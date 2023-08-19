ab072aa0eea22cbe5ff75baa96f81d72c33b4077
Uranbek Anarbaev <W1zarD.11830@mail.ru>
2023-08-19 18:47:53 +0600

    Added Features

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
+        int2 = 2+2
         return Label(text="Привет, мир!")
+    
 
 if __name__ == '__main__':
     MyApp().run()
